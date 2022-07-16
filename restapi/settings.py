3c57b5b1ebe89e3bee4f5ba1050fde9595a88026
Simon Chen <chensihai@gmail.com>
2022-07-16 11:04:15 -0400

    databsae connection

--- restapi/settings.py
+++ restapi/settings.py
@@ -74,10 +74,26 @@ WSGI_APPLICATION = 'restapi.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'db',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'password',
+
+        'HOST': 'database',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
