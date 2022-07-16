2c3d66c756e4a13305d62c506b38be81102ddd21
Simon Chen <chensihai@gmail.com>
2022-07-16 11:13:09 -0400

    more changes

--- restapi/settings.py
+++ restapi/settings.py
@@ -95,7 +95,23 @@ DATABASES = {
     }
 
 }
+# DATABASES = {
 
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'db',
+
+#         'PASSWORD': 'password',
+
+#         'HOST': 'database',
+
+#         'PORT': '5432',
+
+#     }
+
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
