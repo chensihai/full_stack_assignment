a5e15d588e74f887968b621a71a58f98707f25ae
Simon Chen <chensihai@gmail.com>
2022-07-17 10:35:32 -0400

    debug setting

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*o&c0&q=fd4=e!!i!y1420*hb!o4%gbh1k%)^2f#^p2ldnyo5b'
+SECRET_KEY = os.getenv('SEC_KEY')    
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
