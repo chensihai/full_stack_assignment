0ad5c7a05fc374302525922d7e36d771eb552137
Simon Chen <chensihai@gmail.com>
2022-07-18 15:51:05 -0400

    add temple for test

--- app/people/views.py
+++ app/people/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
-from rest_framework import status
-
+# from rest_framework.parsers import JSONParser 
+# from rest_framework import status
 from rest_framework.decorators import api_view
+from people.models import People
 
 # Create your views here.
+# @api_view(['GET'])
+# def index(request):
+#   return JsonResponse({'message': 'people index'}, status=status.HTTP_200_OK)
+
 @api_view(['GET'])
 def index(request):
-  return JsonResponse({'message': 'people index'}, status=status.HTTP_200_OK)
+    people = People.objects.all()[:10]
+
+    return render(request, 'people.html', {'people': people})  
