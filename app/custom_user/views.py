26e337bcaa5efebb17c60b2c16703b69f6d19fd0
Simon Chen <chensihai@gmail.com>
2022-07-21 21:33:38 -0400

    Fix API /api/user/ etc

--- app/custom_user/views.py
+++ app/custom_user/views.py
@@ -1,11 +1,48 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from django.http.response import JsonResponse
-from custom_user.models import CustomUser
-from django.core import serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view 
+from rest_framework.views import APIView 
+from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import ParseError
+from rest_framework import status
+ 
+from django.contrib.auth.models import User
+ 
+ 
+# Create your views here.
+class TestView(APIView):
+    """
+    """
+ 
+    def get(self, request, format=None):
+        return Response({'detail': "GET Response"})
+ 
+    def post(self, request, format=None):
+        try:
+            data = request.DATA
+        except ParseError as error:
+            return Response(
+                'Invalid JSON - {0}'.format(error.detail),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if "user" not in data or "password" not in data:
+            return Response(
+                'Wrong credentials',
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+ 
+        user = User.objects.first()
+        if not user:
+            return Response(
+                'No default user, please create one',
+                status=status.HTTP_404_NOT_FOUND
+            )
+ 
+        token = Token.objects.get_or_create(user=user)
+ 
+        return Response({'detail': 'POST answer', 'token': token[0].key})    
 
 @api_view(['GET'])
 def index(request):
-    user=CustomUser.objects.all()
-    json_res = serializers.serialize('json', user)  
-    return JsonResponse({'msg': json_res}, status=200) 
+    vindex=TestView();
+    return vindex.get(request)
+    
