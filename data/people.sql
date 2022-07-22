74c92fed58f5cf23af6264525a3e81d1243e5c5e
Simon Chen <chensihai@gmail.com>
2022-07-21 20:20:42 -0400

    people data migrate ready

---
+++ data/people.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO public.people_people(
+	id, name, lattitude, longtitude)
+	VALUES (1, 'Simon', 23.8105, 90.4126);
+
+INSERT INTO public.people_people(
+	id, name, lattitude, longtitude)
+	VALUES (2, 'Jenny', 23.8107, 90.4123);
+
+INSERT INTO public.people_people(
+	id, name, lattitude, longtitude)
+	VALUES (3, 'Josh', 23.8102, 90.4124);
+
+INSERT INTO public.people_people(
+	id, name, lattitude, longtitude)
+	VALUES (4, 'Kiki', 23.8106, 90.4122);
+	
+INSERT INTO public.people_people(
+	id, name, lattitude, longtitude)
+	VALUES (5, 'Snowy', 23.8103, 90.4128);
